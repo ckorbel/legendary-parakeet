bcbea67bd6be10bf97af2ccfcc953726d997e22f
Chris Korbel <ckorbel1@gmail.com>
2018-08-31 01:09:06 -0700

    web scraping

--- scrape.js
+++ scrape.js
@@ -1,2 +1,18 @@
 const request = require('request');
-const request = require('cheerio');
+const cheerio = require('cheerio');
+
+request('https://ckorbel.github.io/', (err, res, html) => {
+    if (!err && res.statusCode == 200) {
+        const $ = cheerio.load(html);
+
+        const siteHeading = $('#lead-content')
+
+        // console.log(siteHeading.html());
+        // console.log(siteHeading.text());
+        // const output = siteHeading.find('h1').text();
+        const output = siteHeading.children('h1').next().text();
+
+
+        console.log(output);
+    }
+});
