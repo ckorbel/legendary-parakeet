f8ed2813230d430a777af982b7540662d4e08803
Chris Korbel <ckorbel1@gmail.com>
2018-09-02 09:25:35 -0700

    rankings

--- scrape2.js
+++ scrape2.js
@@ -1,13 +1,17 @@
 const request = require('request');
 const cheerio = require('cheerio');
 
-request('https://ckorbel.github.io/', (err, res, html) => {
+request('http://www.espn.com/college-football/rankings', (err, res, html) => {
     if (!err && res.statusCode == 200) {
         const $ = cheerio.load(html);
 
-        $('.vtimeline-content').each((e, el) => {
+        $('tr').each((e, el) => {
             const title = $(el).text();
-            console.log(title);
+
+            const link = $(el).find('a').attr('href');
+
+
+            console.log(title, date);
         })
 
     }
