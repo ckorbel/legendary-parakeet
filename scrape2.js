eb9be95fdb4d590508c1775003cd7aa019cce130
Chris Korbel <ckorbel1@gmail.com>
2018-09-02 09:34:45 -0700

    ncaa rankings

--- scrape2.js
+++ scrape2.js
@@ -1,5 +1,10 @@
 const request = require('request');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('post.csv');
+
+//Headers 
+writeStream.write(`Title, Link`);
 
 request('http://www.espn.com/college-football/rankings', (err, res, html) => {
     if (!err && res.statusCode == 200) {
@@ -11,8 +16,10 @@ request('http://www.espn.com/college-football/rankings', (err, res, html) => {
             const link = $(el).find('a').attr('href');
 
 
-            console.log(title, date);
-        })
+            writeStream.write(`${title}, ${link} \n`);
+        });
+
+        console.log('scrapping done...');
 
     }
 });
